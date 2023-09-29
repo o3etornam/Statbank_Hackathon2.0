9a01b8b7744064af4e22cb44e7c4e6eceefaefe8
o3etornam <03etornam@gmail.com>
2023-09-29 05:55:25 +0000

    merged page

--- pages/Merge_and_Compare.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from utility import query_builder, convert_df, data_filter
-import pandas as pd
-import plotly.express as px
-import features, warehouse
-
-st.title('Merge and Compare Data Across Various Categories of PHC 2021 Data')
-merge_list = st.sidebar.selectbox('Which PHC 2021 data will you like to visualize', warehouse.categories)
