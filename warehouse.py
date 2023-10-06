bc1b98a9380b9d9beb834e39d39e4e53ba205606
o3etornam <03etornam@gmail.com>
2023-10-06 05:39:43 +0000

    added more categories of PHC data

--- warehouse.py
+++ warehouse.py
@@ -9,6 +9,10 @@ warehouse = {
     'Water and Sanitation': {'Ownership of Laptops':{'extension': 'ICT/ownict_table_4.px','query_path':'Own_laptop.json'},
            'Ownership of Functional Mobile':{'extension':'ICT/ownmobile_table.px','query_path':'Own_functional_mobile.json'},
            },
+     'Difficulties in Performing Activities':{},
+     'Education and Literacy':{},
+     
+
     
 
 }
